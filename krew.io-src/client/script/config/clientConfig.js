7466e1a65ce4d7e93b23bf609622de74c686e2bd
Marcie Lakes <75328497+Crxscent@users.noreply.github.com>
2022-06-06 00:01:02 -0500

    Add files via upload

---
+++ krew.io-src/client/script/config/clientConfig.js
@@ -0,0 +1,35 @@
+const config = {
+    // Client side staff
+    Admins: [`DamienVesper`, `Zeropoint`], // I still love ahan even though we dont talk much. The others.. I really didn't care about much... I know why too
+    Mods: [`TheRandomguy98`],
+    Helpers: [`Wartowin`],
+    Designers: [`Zeropoint`],
+
+    // Set URL
+    url: `https://example.com`,
+
+    // Set config.worldsize
+    worldsize: 2500,
+
+    // Set islands for decorations
+    palmTree: [`Jamaica`],
+    christmasTree: [],
+    snowman: [],
+
+    // Misc config
+    setProperties: {
+        inVision: false
+    },
+    Labels: {
+        redrawInterval: 250,
+        fontFamily: `Arial, Helvetica, sans-serif`,
+        distanceMultiplier: {
+            0: 40,
+            1: 160,
+            5: 300
+        },
+        boats: {
+            useMethod: `inRange` // `inVision` or `inRange`
+        }
+    }
+};
