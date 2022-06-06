5345efee2476a65679bc53bfd5ae2e3a67e0f54b
Marcie Lakes <75328497+Crxscent@users.noreply.github.com>
2022-06-06 00:30:01 -0500

    Delete krew.io-src/server directory

--- krew.io-src/server/core/item.js
+++
@@ -1,8 +0,0 @@
-// PLayers are entities, check core_entity.js for the base class
-Item.prototype.constructor = Item;
-
-function Item () {}
-
-Item.prototype.logic = function (dt) {
-
-};
--- krew.io-src/server/core/preConcat.js
+++
@@ -1,8 +0,0 @@
-let THREE = require(`../../client/libs/js/three.min.js`);
-let SERVER = true;
-
-let log = require(`../utils/log.js`);
-
-let User = require(`../models/user.model.js`);
-let Clan = require(`../models/clan.model.js`);
-let Ban = require(`../models/ban.model.js`);
